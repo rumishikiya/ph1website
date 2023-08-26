d3b0dc0421006f4195a06bb723459a0c8d7c1166
rumishikiya <rumi.shikiya@gmail.com>
2023-08-27 01:08:18 +0900

    week11途中

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//ボタンを一度押したらクラスを付与し、もう一度押したらクラスが解除されるようにしたい
+//ヘッダー、ボタンの要素を取得
+const header = document.getElementById('js-header');
+const button = document.getElementById('js-headerButton');
+
+// ボタンをクリックした時の処理用の関数を定義
+function jsHeaderButtonClick() {
+    console.log('実行できています。');
+}
+
+// ボタンのクリックイベントに関数を割り当て
+button.addEventListener('click', jsHeaderButtonClick);
+
+// クラスのトグル処理も追加
+button.addEventListener('click', () => {
+    button.classList.toggle('is-open');
+    console.log(button);
+});
+
